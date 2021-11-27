132fb5f4c3d5bc531fc1a70188f362f475ecd338
zengshuli <zengshuli0130@mail.ustc.edu.cn>
2021-11-27 16:17:42 +0800

    task2 partly completed

--- Student/task2/cpp/func_gen.cpp
+++ Student/task2/cpp/func_gen.cpp
@@ -1,3 +1,81 @@
-int main(){
+#include "BasicBlock.h"
+#include "Constant.h"
+#include "Function.h"
+#include "IRStmtBuilder.h"
+#include "Module.h"
+#include "Type.h"
+
+#include <iostream>
+#include <memory>
+#ifdef DEBUG                                             // 用于调试信息,大家可以在编译过程中通过" -DDEBUG"来开启这一选项
+#define DEBUG_OUTPUT std::cout << __LINE__ << std::endl; // 输出行号的简单示例
+#else
+#define DEBUG_OUTPUT
+#endif
+
+#define CONST_INT(num) \
+    ConstantInt::get(num, module)
+
+#define CONST_FP(num) \
+    ConstantFP::get(num, module) // 得到常数值的表示,方便后面多次用到
+int main()
+{
+    auto module = new Module("SysY code"); // module name是什么无关紧要
+    auto builder = new IRStmtBuilder(nullptr, module);
+    Type *Int32Type = Type::get_int32_type(module);
+    // add函数
+    // 函数参数类型的vector
+    std::vector<Type *> Ints(2, Int32Type);
+
+    //通过返回值类型与参数类型列表得到函数类型
+    auto addFunTy = FunctionType::get(Int32Type, Ints);
+
+    // 由函数类型得到函数
+    auto addFun = Function::create(addFunTy,
+                                   "add", module);
+    // BB的名字在生成中无所谓,但是可以方便阅读
+    auto bb = BasicBlock::create(module, "entry", addFun);
+
+    builder->set_insert_point(bb); // 一个BB的开始,将当前插入指令点的位置设在bb
+
+    auto retAlloca = builder->create_alloca(Int32Type); // 在内存中分配返回值的位置
+    auto aAlloca = builder->create_alloca(Int32Type);   // 在内存中分配参数a的位置
+    auto bAlloca = builder->create_alloca(Int32Type);   // 在内存中分配参数b的位置
+
+    std::vector<Value *> args; // 获取add函数的形参,通过Function中的iterator
+    for (auto arg = addFun->arg_begin(); arg != addFun->arg_end(); arg++)
+    {
+        args.push_back(*arg); // * 号运算符是从迭代器中取出迭代器当前指向的元素
+    }
+
+    builder->create_store(args[0], aAlloca);    // store参数a
+    builder->create_store(args[1], bAlloca);    // store参数b
+    auto aLoad = builder->create_load(aAlloca); // 将参数a load上来
+    auto bLoad = builder->create_load(bAlloca); // 将参数b load上来
+    auto add = builder->create_iadd(aAlloca, bAlloca);
+    auto sub = builder->create_isub(add, CONST_INT(1));
+    builder->create_ret(sub);
+    // main函数
+    auto mainFun = Function::create(FunctionType::get(Int32Type, {}),
+                                    "main", module);
+    bb = BasicBlock::create(module, "entry", mainFun);
+    // BasicBlock的名字在生成中无所谓,但是可以方便阅读
+    builder->set_insert_point(bb);
+    retAlloca = builder->create_alloca(Int32Type);
+    aAlloca = builder->create_alloca(Int32Type);
+    bAlloca = builder->create_alloca(Int32Type); // 在内存中分配
+    auto cAlloca = builder->create_alloca(Int32Type);
+    builder->create_store(CONST_INT(0), retAlloca);
+    builder->create_store(CONST_INT(3), aAlloca);
+    builder->create_store(CONST_INT(2), bAlloca);
+    builder->create_store(CONST_INT(5), cAlloca);
+    aLoad = builder->create_load(aAlloca);
+    bLoad = builder->create_load(bAlloca);
+    auto cLoad = builder->create_load(cAlloca);
+    auto call = builder->create_call(addFun, {aLoad, bLoad});
+    auto retLoad = builder->create_iadd(cLoad, call);
+    builder->create_ret(retLoad);
+    std::cout << module->print();
+    delete module;
     return 0;
 }
--- Student/task2/cpp/if_gen.cpp
+++ Student/task2/cpp/if_gen.cpp
@@ -1,3 +1,59 @@
-int main(){
+#include "BasicBlock.h"
+#include "Constant.h"
+#include "Function.h"
+#include "IRStmtBuilder.h"
+#include "Module.h"
+#include "Type.h"
+
+#include <iostream>
+#include <memory>
+
+#ifdef DEBUG                                             // 用于调试信息,大家可以在编译过程中通过" -DDEBUG"来开启这一选项
+#define DEBUG_OUTPUT std::cout << __LINE__ << std::endl; // 输出行号的简单示例
+#else
+#define DEBUG_OUTPUT
+#endif
+
+#define CONST_INT(num) \
+    ConstantInt::get(num, module)
+
+#define CONST_FP(num) \
+    ConstantFP::get(num, module) // 得到常数值的表示,方便后面多次用到
+
+int main()
+{
+    auto module = new Module("SysY code"); // module name是什么无关紧要
+    auto builder = new IRStmtBuilder(nullptr, module);
+    Type *Int32Type = Type::get_int32_type(module);
+
+    // 全局a
+    auto zero_initializer = ConstantZero::get(Int32Type, module);
+    auto a = GlobalVariable::create("a", module, Int32Type, false, zero_initializer);
+    auto mainFun = Function::create(FunctionType::get(Int32Type, {}), "main", module);
+
+    auto bb = BasicBlock::create(module, "entry", mainFun);
+    // BasicBlock的名字在生成中无所谓,但是可以方便阅读
+    builder->set_insert_point(bb);
+    auto retAlloca = builder->create_alloca(Int32Type);
+    builder->create_store(CONST_INT(0), retAlloca);
+    builder->create_store(CONST_INT(10), a);
+    auto aLoad = builder->create_load(a);                             // 将全局变量a load上来
+    auto icmp = builder->create_icmp_lt(aLoad, CONST_INT(0));         // a和0的比较
+    auto retBB = BasicBlock::create(module, "", mainFun);             // return分支,提前create,以便true分支可以br
+    auto trueBB = BasicBlock::create(module, "trueBB_if", mainFun);   // true分支
+    auto falseBB = BasicBlock::create(module, "falseBB_if", mainFun); // false分支
+    builder->create_cond_br(icmp, trueBB, falseBB);                   // 条件BR
+    builder->set_insert_point(trueBB);                                // if true; 分支的开始需要SetInsertPoint设置
+    aLoad = builder->create_load(a);
+    builder->create_store(aLoad, retAlloca);
+    builder->create_br(retBB);          // br retBB
+    builder->set_insert_point(falseBB); // if false
+    builder->create_store(CONST_INT(0), retAlloca);
+    builder->create_br(retBB);
+    builder->set_insert_point(retBB); // ret分支
+    auto retLoad = builder->create_load(retAlloca);
+    builder->create_ret(retLoad);
+    std::cout << module->print();
+    delete module;
     return 0;
 }
